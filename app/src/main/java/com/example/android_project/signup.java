59c018bde32aee1922110cfbb58cfe2e2220e91c
mayankShiroya <mshiroya1369@conestogac.on.ca>
2022-12-10 11:44:22 -0500

    Made OTP screen and otp verification

--- app/src/main/java/com/example/android_project/signup.java
+++ app/src/main/java/com/example/android_project/signup.java
@@ -16,12 +16,31 @@ public class signup extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         ImageButton backarrow = (ImageButton) findViewById(R.id.backarrow);
+        Button Login = (Button) findViewById(R.id.Already_login);
+        Button OTP = (Button) findViewById(R.id.otpPage);
+
 
         backarrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(signup.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Already = new Intent(signup.this, MainActivity.class);
+                startActivity(Already);
+            }
+        });
+
+        OTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Already = new Intent(signup.this, opt.class);
+                startActivity(Already);
             }
         });
     }
